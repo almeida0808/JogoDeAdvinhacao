7a948f42534310ead0c884b2809804be17e466d6
almeida0808 <almeidaff08@gmail.com>
2023-11-14 23:36:12 -0300

    1

---
+++ script.js
@@ -0,0 +1,52 @@
+const randomNum = Math.round(Math.random() * 10)
+let numTentativas = 1
+const main = document.querySelector('main')
+
+const video = document.querySelector("main video")
+
+function clicar(event){
+event.preventDefault()
+
+const numInput = document.querySelector("#numInput");
+
+if(Number(numInput.value) === randomNum){
+    var confettiSettings = { target: 'my-canvas' };
+    var confetti = new ConfettiGenerator(confettiSettings);
+    confetti.render();
+
+    document.querySelector(".tela1").classList.add("hide")
+    document.querySelector(".tela2").classList.remove("hide")
+
+main.style.border = "3px solid rgba(0, 255, 42, 0.37)";
+
+if(numTentativas == 1){
+document.querySelector(".tela2 h2").innerText = `Você acertou em ${numTentativas} tentativa`
+
+}else{
+document.querySelector(".tela2 h2").innerText = `Você acertou em ${numTentativas} tentativas`
+}
+}else{
+    document.querySelector(".tela1 h3").innerText = "(Não foi dessa vez tente novamente)"
+}
+
+numTentativas++
+}
+
+
+
+
+
+//CLICAR NO BOTÃO PRA ENVIAR A RESPOSTA
+const btnEnviar = document.querySelector("#button1")
+btnEnviar.addEventListener('click', clicar)
+
+//CLICAR NO BOTÃO PRA JOGAR NOVAMENTE
+const btnReset = document.querySelector("#button2")
+
+btnReset.addEventListener('click' , voltar)
+
+function voltar(event){
+    document.querySelector(".tela2").classList.add("hide")
+    document.querySelector(".tela1").classList.remove("hide")
+    
+}
